163cd429939fea6dc918933dd7d1d50f9dcd117b
DELL <minhduc.59h3@gmail.com>
2026-05-06 14:05:23 +0700

    Bổ xung Controller

--- Controller/WorkController.js
+++ Controller/WorkController.js
@@ -1,4 +1,14 @@
 import { Work } from "../Model/Work";
 import { WorkRepository } from "../Repository/WorkRepository";
 
-export class WorkController {}
+export class WorkController {
+  #Repo = new WorkRepository();
+
+  viewWork() {}
+
+  addWork() {}
+
+  updateWork() {}
+
+  deleteWork() {}
+}
